3bf02a3765f3930837f5a5f1da3cdab8e6908b8c
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-26 20:54:09 -0300

    Add Endpoint para retornar os posts de um usuario

--- src/main/java/com/rodrigo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rodrigo/workshopmongo/config/Instantiation.java
@@ -37,7 +37,7 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paul. Abraços", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"),  "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1,post2));
         maria.getPosts().addAll(Arrays.asList(post1,post2));

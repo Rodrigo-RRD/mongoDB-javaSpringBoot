16efffa2548b39a8ee999ab8ba5f23f5b4d8a411
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-25 22:29:10 -0300

    Insercao de usuario com POST

--- src/main/java/com/rodrigo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rodrigo/workshopmongo/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.rodrigo.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigo.workshopmongo.domain.User;
 import com.rodrigo.workshopmongo.dto.UserDTO;
@@ -21,16 +25,24 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @RequestMapping(method=RequestMethod.GET) // ou pode colocar Getmapping
+    @RequestMapping(method = RequestMethod.GET) // ou pode colocar Getmapping
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto); // no corpo da minha resposta vai ter o body que salvou
     }
 
-    @RequestMapping(value="/{id}", method = RequestMethod.GET) // ou pode colocar Getmapping
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET) // ou pode colocar Getmapping
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/rodrigo/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigo/workshopmongo/services/UserService.java
@@ -7,23 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.workshopmongo.domain.User;
+import com.rodrigo.workshopmongo.dto.UserDTO;
 import com.rodrigo.workshopmongo.repository.UserRepository;
 import com.rodrigo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-    
-//quando declara um objeto dentro de uma 
-//classe usando o Autowired o proprio spring vai procurar 
-//a definição desse objeto e vai instanciar ele (Injeção de dependencia) do spring
+
+    // quando declara um objeto dentro de uma
+    // classe usando o Autowired o proprio spring vai procurar
+    // a definição desse objeto e vai instanciar ele (Injeção de dependencia) do
+    // spring
     @Autowired
     private UserRepository userJpaRepository;;
 
-    public List<User> findAll(){
-    return userJpaRepository.findAll();  //retorna todos os objetos do tipo usuario
+    public List<User> findAll() {
+        return userJpaRepository.findAll(); // retorna todos os objetos do tipo usuario
     }
+
     public User findById(String id) {
         Optional<User> obj = userJpaRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-        }
+    }
+
+    public User insert(User obj) {
+        return userJpaRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+    }
 }

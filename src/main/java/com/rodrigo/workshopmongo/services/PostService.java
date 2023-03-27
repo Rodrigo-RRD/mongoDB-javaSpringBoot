200a1de3661da30b6e6e2b8109ad95da1713dab6
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-27 00:14:34 -0300

    Consulta com varios criterios

--- src/main/java/com/rodrigo/workshopmongo/services/PostService.java
+++ src/main/java/com/rodrigo/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.rodrigo.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +25,9 @@ private PostRepository repo;
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
     }
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
